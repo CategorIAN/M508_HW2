e07edb121ea79ca3d53a650b3fe91d8fee98fc78
CategorIAN <ianholm9@gmail.com>
2024-03-11 15:29:34 -0600

    Reduced MNist Code.

--- MnistDataloader.py
+++ MnistDataloader.py
@@ -1,47 +1,52 @@
-#
-# This is a sample Notebook to demonstrate how to read "MNIST Dataset"
-#
 import numpy as np  # linear algebra
 import struct
 from array import array
-from os.path import join
+import os
+import random
+import matplotlib.pyplot as plt
 
-
-#
-# MNIST Data Loader Class
-#
 class MnistDataloader(object):
-    def __init__(self, training_images_filepath, training_labels_filepath,
-                 test_images_filepath, test_labels_filepath):
-        self.training_images_filepath = training_images_filepath
-        self.training_labels_filepath = training_labels_filepath
-        self.test_images_filepath = test_images_filepath
-        self.test_labels_filepath = test_labels_filepath
+    def __init__(self):
+        names = ["train-images-idx3-ubyte", "train-labels-idx1-ubyte", "t10k-images-idx3-ubyte", "t10k-labels-idx1-ubyte"]
+        files = ["\\".join([os.getcwd(), "archive", name, name]) for name in names]
+        self.train_images_file, self.train_labels_file, self.test_images_file, self.test_labels_file = files
 
-    def read_images_labels(self, images_filepath, labels_filepath):
-        labels = []
-        with open(labels_filepath, 'rb') as file:
+    def read_images_labels(self, images_file, labels_file):
+        with open(labels_file, 'rb') as file:
             magic, size = struct.unpack(">II", file.read(8))
             if magic != 2049:
                 raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
             labels = array("B", file.read())
 
-        with open(images_filepath, 'rb') as file:
+        with open(images_file, 'rb') as file:
             magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
             if magic != 2051:
                 raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
             image_data = array("B", file.read())
-        images = []
-        for i in range(size):
-            images.append([0] * rows * cols)
-        for i in range(size):
-            img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
-            img = img.reshape(28, 28)
-            images[i][:] = img
-
+        image_func = lambda i: np.array(image_data[i * rows * cols:(i + 1) * rows * cols]).reshape(28, 28)
+        images = [image_func(i) for i in range(size)]
         return images, labels
 
     def load_data(self):
-        x_train, y_train = self.read_images_labels(self.training_images_filepath, self.training_labels_filepath)
-        x_test, y_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
+        x_train, y_train = self.read_images_labels(self.train_images_file, self.train_labels_file)
+        x_test, y_test = self.read_images_labels(self.test_images_file, self.test_labels_file)
         return (x_train, y_train), (x_test, y_test)
+
+    def show_images(self):
+        (x_train, y_train), (x_test, y_test) = self.load_data()
+        def index_image_title(i):
+            if i < 10:
+                r = random.randint(1, 60000)
+                return i + 1, x_train[r], "train [{}] = {}".format(r, y_train[r])
+            else:
+                r = random.randint(1, 10000)
+                return i + 1, x_test[r], "test [{}] = {}".format(r, y_test[r])
+
+        size, cols = 15, 5
+        rows = int(size / cols) + 1
+        plt.figure(figsize=(30, 20))
+        for index, image, title in [index_image_title(i) for i in range(15)]:
+            plt.subplot(rows, cols, index)
+            plt.title(title, fontsize=15)
+            plt.imshow(image, cmap="gray", vmin=0, vmax=255)
+        plt.show()
