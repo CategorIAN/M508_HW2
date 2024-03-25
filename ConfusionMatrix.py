a2ca1a2e3410830bdd777d3a648c7df70324fe12
CategorIAN <ianholm9@gmail.com>
2024-03-25 14:51:00 -0600

    ..

--- ConfusionMatrix.py
+++ ConfusionMatrix.py
@@ -2,17 +2,27 @@ import pandas as pd
 import numpy as np
 
 class ConfusionMatrix:
-    def __init__(self, matrix):
+    def __init__(self, matrix = None, file = None):
         '''
         :param matrix: A numpy array of the confusion matrix
         '''
-        self.matrix = matrix
+        self.matrix = np.array(pd.read_csv(file, index_col=0)) if matrix is None else matrix
 
     def df(self):
         '''
         :return: A pandas dataframe representation of the confusion matrix
         '''
         return pd.DataFrame(self.matrix, columns=["ActualTrue", "ActualFalse"], index=["PredTrue", "PredFalse"])
+
+    def updated(self, predicted, actual):
+        '''
+        :param predicted: Boolean of the predicted
+        :param actual: Boolean of the actual
+        :return: Confusion matrix with one entry added by one
+        '''
+        unitvector = lambda boolean: np.array([int(boolean), int(not boolean)])
+        m = np.outer(unitvector(predicted), unitvector(actual))
+        return ConfusionMatrix(self.matrix + m)
 
     def __str__(self):
         return str(self.df())
@@ -27,12 +37,3 @@ class ConfusionMatrix:
     def recall(self):
         return self.matrix[0][0] / sum(self.matrix[:, 0])
 
-    def updated(self, predicted, actual):
-        '''
-        :param predicted: Boolean of the predicted
-        :param actual: Boolean of the actual
-        :return: Confusion matrix with one entry added by one
-        '''
-        unitvector = lambda boolean: np.array([int(boolean), int(not boolean)])
-        m = np.outer(unitvector(predicted), unitvector(actual))
-        return ConfusionMatrix(self.matrix + m)
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from NaiveBayes import NaiveBayes
 from GDA import GDA
 from ConfusionMatrix import ConfusionMatrix
 from Analysis import Analysis
+import os
+import warnings
+warnings.filterwarnings('ignore')
 
 
 def f(i):
@@ -17,10 +20,19 @@ def f(i):
         train, test = M.zero_one_train, M.zero_one_test
         A = Analysis(train, test, "GDA")
         print(A.finalConfMat())
+    if i == 3:
+        m = np.array([[1, 1], [1, 1]])
+        M = ConfusionMatrix(m)
+        print(M)
+    if i == 4:
+        file = "\\".join([os.getcwd(), "Analysis", "GDAConfusionMatrix.csv"])
+        M = ConfusionMatrix(None, file)
+        print(M)
+        print(M.error())
 
 
 if __name__ == '__main__':
-    f(2)
+    f(1)
 
 
 
